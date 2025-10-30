262f4b61182739fb4ad14db78c4515f68ec4dda2
Nahar <naharkalitaadtu09@gmail.com>
2025-10-30 15:55:55 +0000

    We will push these changes later in the lab.

--- example.py
+++ example.py
@@ -3,3 +3,8 @@ def git_opeation():
  print("I am adding example.py file to the remote repository.")
 
 git_opeation()
+def git_opeation():
+
+ print("I am adding example.py file to the remote repository.")
+
+git_opeation()
